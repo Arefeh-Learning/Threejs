2782c43550cfc906a0835285cadb5fabac41b878
Arefeh Kompani <awriik00@gmail.com>
2023-03-25 04:06:47 +0430

    Transform and Rotate a mesh

--- 02 - Webpack/src/script.js
+++ 02 - Webpack/src/script.js
@@ -10,7 +10,24 @@ const material = new THREE.MeshBasicMaterial({
     color: 'red'
 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
+
 scene.add(mesh)
+// You can normalize its values
+// mesh.position.normalize()
+// console.log(mesh.position.length()) // log 1
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 // Sizes
 const sizes = {
@@ -21,8 +38,12 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 2
 scene.add(camera)
+
+camera.lookAt(mesh.position)
+
+// We can get the distance from another Vector3
+// console.log(mesh.position.distanceTo(camera.position))
 
 // Renderer
 const canvas = document.querySelector('.webgl')
