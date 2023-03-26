8c6c0e17b354c83834e05ce67ccdc6b5fefb196b
Arefeh Kompani <awriik00@gmail.com>
2023-03-26 17:03:30 +0430

    Change poisiton of camera to see different angle

--- 03 - Animation/src/script.js
+++ 03 - Animation/src/script.js
@@ -5,6 +5,18 @@ import gsap from 'gsap'
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Cursor
+*/
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5 // give coordinate
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
@@ -12,8 +24,8 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Axes Helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 // Sizes
 const sizes = {
@@ -22,8 +34,18 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRatio, 
+//     1 * aspectRatio, 
+//     1, 
+//     -1, 
+//     0.1, 
+//     100
+// )
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -34,17 +56,23 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Clock
-// const clock = new THREE.Clock()
+const clock = new THREE.Clock()
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+// gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
 // Animations
 const tick = () => {
     // Clock
-    // const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()
+    
+    // Update camera
+    camera.position.x = Math.sin(cursor.x * 10) * 3
+    camera.position.z = Math.cos(cursor.x * 10) * 3
+    camera.lookAt(new THREE.Vector3()) // or mesh.position
 
     // Update objects
-    // mesh.position.x = Math.sin(elapsedTime)
+    // mesh.rotation.y = elapsedTime
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
