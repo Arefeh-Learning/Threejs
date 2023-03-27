948188cae11ef591a8b361af159f8c7cf5de4152
Arefeh Kompani <awriik00@gmail.com>
2023-03-27 22:41:32 +0430

    Add border to cube

--- 03 - Animation/src/script_animation_camera.js
+++ 03 - Animation/src/script_animation_camera.js
@@ -24,23 +24,29 @@ window.addEventListener('mousemove', (event) => {
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
-
-var geo = new THREE.EdgesGeometry( mesh.geometry );
-var mat = new THREE.LineBasicMaterial( { color: 0x000000 } );
-var wireframe = new THREE.LineSegments( geo, mat );
-mesh.add( wireframe );
 scene.add(mesh)
 
+// Axes Helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 // Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: 800,
+    height: 600
 }
 
 // Camera
 const aspectRatio = sizes.width / sizes.height
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRatio, 
+//     1 * aspectRatio, 
+//     1, 
+//     -1, 
+//     0.1, 
+//     100
+// )
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -58,10 +64,19 @@ renderer.setSize(sizes.width, sizes.height)
 // Clock
 const clock = new THREE.Clock()
 
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+// gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
 // Animations
 const tick = () => {
     // Clock
     const elapsedTime = clock.getElapsedTime()
+    
+    // Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(new THREE.Vector3()) // or mesh.position
 
     // Update controls when using damping
     controls.update()
