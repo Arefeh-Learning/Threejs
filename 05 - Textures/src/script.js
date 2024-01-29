196926804aee871a64154b1de156bddf187be53a
arefehkompani <awriik00@gmail.com>
2024-01-29 22:49:18 +0330

    Add texture to cube

--- 05 - Textures/src/script.js
+++ 05 - Textures/src/script.js
@@ -1,78 +1,58 @@
-import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import * as dat from 'dat.gui'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
- * Debug
+ * Textures
 */
-const gui = new dat.GUI({ closed: true })
-
-const parameters = {
-    color: 0xfff000,
-    spin: () => {
-        gsap.to(mesh.rotation, {duration: 1, y: mesh.rotation.y + 10})
-    }
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () => {
+    console.log('onStart');
 }
+loadingManager.onLoad = () => {
+    console.log('onLoad');
+}
+loadingManager.onProgress = () => {
+    console.log('onProgress');
+}
+loadingManager.onError = () => {
+    console.log('onError');
+}
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 /**
  * Base
-*/
-//  Canvas
-const canvas = document.querySelector('.webgl')
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
 /**
- * Cursor
-*/
-const cursor = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5 // give coordinate
-    cursor.y = -(event.clientY / sizes.height - 0.5)
-})
-
-// Object
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial( {color: parameters.color} )
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Debug
-gui
-    .add(mesh.position, 'y')
-    .min(-3)
-    .max(3)
-    .step(0.01)
-    .name('elevation')
-
-gui 
-    .add(mesh, 'visible')
-
-gui
-    .add(mesh.material, 'wireframe')
-
-gui
-    .addColor(parameters, 'color')
-    .onChange(() => {
-        material.color.set(parameters.color)
-    })
-
-gui
-    .add(parameters, 'spin')
-
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () =>
+{
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -83,66 +63,49 @@ window.addEventListener('resize', () => {
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-window.addEventListener('dblclick', () => {
-    // For all browser 
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-    if (!fullscreenElement) {
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen()
-        } else if (canvas.webkitFullscreenElement) {
-            canvas.webkitFullscreenElement()
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen()
-        } else if (document.webkitExitFullscreen) {
-            document.exitFullscreen()
-        }
-    }
-    // For all browser except safari
-    // if (!document.fullscreenElement) {
-    //     canvas.requestFullscreen()
-    // } else {
-    //     document.exitFullscreen()
-    // }
-})
-// Camera
-const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
-camera.position.z = 2
-camera.lookAt(mesh.position)
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// Clock
+
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
-// Animations
-const tick = () => {
-    // Clock
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
 
-    // Update controls when using damping
+    // Update controls
     controls.update()
 
-    // Update objects
-    // mesh.rotation.y = elapsedTime
+    // Render
     renderer.render(scene, camera)
+
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
 tick()
-
-
- 
